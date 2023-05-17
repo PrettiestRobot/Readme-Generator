d3ef4ded9e7ccf7af5cd5333c9b8c14b9f147ccd
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-05-17 00:17:27 -0400

    questions added to array

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    "Please enter your project desctription:", "Please enter any installation instructions:", "Input project installation instructions:", "Please enter any necesary usage instructions:",  "Please select a liscense:", "Please list contribution instructions:", "Please supply your github profile address:", "Please supply your email:"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
