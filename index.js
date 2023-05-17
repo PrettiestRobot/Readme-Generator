d5d247dd797829391cce4967912e626d3f848496
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-05-17 01:24:26 -0400

    inquirer prompting user

--- index.js
+++ index.js
@@ -4,8 +4,55 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
     // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-    "Please enter your project desctription:", "Please enter any installation instructions:", "Input project installation instructions:", "Please enter any necesary usage instructions:",  "Please select a liscense:", "Please list contribution instructions:", "Please supply your github profile address:", "Please supply your email:"
+    "Please enter your project desctription:",
+    "Input project installation instructions:",
+    "Please enter any necesary usage instructions:",
+    "Please select a liscense:", "Please list contribution instructions:",
+    "Please supply your github profile address:",
+    "Please supply your email:"
 ];
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: questions[0],
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: questions[1],
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: questions[2],
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: questions[3],
+        name: 'liscense',
+        choices: ['MIT']
+    },
+    {
+        type: 'input',
+        message: questions[4],
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: questions[5],
+        name: 'questions github'
+    },
+    {
+        type: 'email',
+        message: questions[6],
+        name: 'questions email'
+    },
+    
+]).then((data) => {
+    console.log(data);
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
